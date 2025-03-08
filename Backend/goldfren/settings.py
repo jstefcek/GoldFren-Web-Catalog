0a39d8909fb1999bc07c32c9c729569ebbbf6e4f
Jakub Štefček <106766680+TrixCZE@users.noreply.github.com>
2025-03-08 18:48:26 +0100

    Změna verze Python na 3.13.2
    Změna verze Python na 3.13.3 + změny dockeru a příprava pro React frontend

--- Backend/goldfren/settings.py
+++ Backend/goldfren/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'goldfren.urls'
@@ -70,6 +74,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'goldfren.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
