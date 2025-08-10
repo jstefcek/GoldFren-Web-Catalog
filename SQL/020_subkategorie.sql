acd193fe49a75c594063e37bae364ebc1d34c5f8
Jakub Štefček <jakub.stefcek@hotmail.cz>
2025-08-10 20:31:58 +0200

    SQL data fix

--- SQL/020_subkategorie.sql
+++ SQL/020_subkategorie.sql
@@ -1,24 +1,24 @@
-INSERT INTO goldfren_data.c_subkategorie (kod,kategorie,nazev,nazev_eng) VALUES
-	 (1,1,'Dirt','Dirt'),
-	 (2,1,'Street','Street'),
-	 (3,1,'Atv','Atv'),
-	 (4,1,'Racing','Racing'),
+INSERT INTO c_subkategorie (kod,kategorie,nazev,nazev_eng) VALUES
+	 (1,1,'Motocross','Dirt'),
+	 (2,1,'Silniční','Street'),
+	 (3,1,'ATV','ATV'),
+	 (4,1,'Závodní','Racing'),
 	 (5,1,'Bike','Bike'),
 	 (6,1,'Nezařazeno','Unclassified'),
 	 (7,1,'Motard','Motard'),
 	 (8,2,'Nezařazeno','Unclassified'),
-	 (9,2,'Passenger','Passenger'),
-	 (10,2,'Racing','Racing');
-INSERT INTO goldfren_data.c_subkategorie (kod,kategorie,nazev,nazev_eng) VALUES
-	 (11,2,'Truck','Truck'),
+	 (9,2,'Osobní','Passenger'),
+	 (10,2,'Závodní','Racing');
+INSERT INTO c_subkategorie (kod,kategorie,nazev,nazev_eng) VALUES
+	 (11,2,'Kamión','Truck'),
 	 (12,3,'Nezařazeno','Unclassified'),
-	 (13,3,'Mountain','Mountain'),
+	 (13,3,'Horské kolo','Mountain'),
 	 (14,3,'Frames','Frames'),
 	 (15,4,'Nezařazeno','Unclassified'),
 	 (16,4,'-','-'),
 	 (17,5,'Nezařazeno','Unclassified'),
 	 (18,5,'-','-'),
 	 (19,6,'Nezařazeno','Unclassified'),
-	 (20,6,'Racing','Racing');
-INSERT INTO goldfren_data.c_subkategorie (kod,kategorie,nazev,nazev_eng) VALUES
+	 (20,6,'Závodní','Racing');
+INSERT INTO c_subkategorie (kod,kategorie,nazev,nazev_eng) VALUES
 	 (21,1,'Sněžný skůtr','Snow mobile');
