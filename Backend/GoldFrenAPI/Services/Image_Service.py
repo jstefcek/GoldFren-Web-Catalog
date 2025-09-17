ae29874560ecc3f24af25490aab8b2e9daf1318e
Jakub Štefček <jakub.stefcek@hotmail.cz>
2025-09-17 18:27:55 +0200

    Simplified image upload

--- Backend/GoldFrenAPI/Services/Image_Service.py
+++ Backend/GoldFrenAPI/Services/Image_Service.py
@@ -7,9 +7,8 @@ def save_image_file(sortiment: str, file_object, file_type: str, component_id: s
     if file_type not in ['image', 'vector']:
         raise ValueError("Invalid file type. Must be 'image' or 'vector'.")
 
-    # Gets media category from the database and validates it
-    media_category = get_sortiment_image_category(sortiment)
-    if not media_category:
+    # Gets sortiment
+    if not sortiment:
         raise ValueError("Sortiment does not exist in the database.")
 
     # Validate file file extension
@@ -26,13 +25,13 @@ def save_image_file(sortiment: str, file_object, file_type: str, component_id: s
     # Use component_id as filename
     filename = f"{component_id}{ext}"
     
-    dir_path = os.path.join(settings.MEDIA_ROOT, media_category, file_type)
+    dir_path = os.path.join(settings.MEDIA_ROOT, sortiment, file_type)
     file_path = os.path.join(dir_path, filename)
 
     # Create the directory and save the file
     os.makedirs(dir_path, exist_ok=True)
     
-    # Remove existing file with different extension if it exists
+    # Remove existing file with different extension if it exists 
     for existing_ext in ['.jpg', '.jpeg', '.png', '.svg']:
         if existing_ext != ext:
             existing_file = os.path.join(dir_path, f"{component_id}{existing_ext}")
@@ -44,20 +43,4 @@ def save_image_file(sortiment: str, file_object, file_type: str, component_id: s
             file.write(chunk)
 
     # Return the URL of the saved file
-    return f"{settings.MEDIA_URL}{media_category}/{file_type}/{filename}"
-
-def get_sortiment_image_category(sortiment: str) -> str:
-    """This function retrieves the image category for a given sortiment from the database.""" 
-    conn = connect()
-    if conn is not None:
-        # Create a cursor and execute the query
-        cursor = conn.cursor()
-        cursor.execute("SELECT image_categories FROM c_sortiment WHERE nazev = %s", (sortiment,))
-        record = cursor.fetchone()
-        cursor.close()
-        conn.close()
-        # Returns image category if it exists, otherwise None
-        return record["image_categories"] if record and record["image_categories"] else None
-    else:
-        print("Connection failed")
-        return None
+    return f"{settings.MEDIA_URL}{sortiment}/{file_type}/{filename}"
