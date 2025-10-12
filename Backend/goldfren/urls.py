d34cb56de3af0c176269f6339ae0ac0fdc31434d
Jakub Štefček <jakub.stefcek@hotmail.cz>
2025-10-12 16:14:54 +0200

    Uprava cesty pro Django admin stránku

--- Backend/goldfren/urls.py
+++ Backend/goldfren/urls.py
@@ -3,13 +3,8 @@ URL configuration for GoldFren project.
 """
 from django.contrib import admin
 from django.urls import include, path
-from GoldFrenAPI.Authentication.Auth_View import (
-    Login_View, 
-    Register_User,
-    change_user_password
-)
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/goldfren/internal/', include('GoldFrenAPI.urls')),
 ]
