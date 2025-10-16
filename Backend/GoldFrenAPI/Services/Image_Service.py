2ff21c6dff63ce130fd2ea2df8362f04f4462123
Jakub Štefček <jakub.stefcek@hotmail.cz>
2025-10-16 22:07:35 +0200

    Python package bump to newer versions and new file optimize function

--- Backend/GoldFrenAPI/Services/Image_Service.py
+++ Backend/GoldFrenAPI/Services/Image_Service.py
@@ -1,8 +1,9 @@
 import os
 from django.conf import settings
-from Components.MySQL import connect
+from PIL import Image
 
-def save_image_file(sortiment: str, file_object, file_type: str, component_id: str):
+def save_image_file(sortiment: str, file_object, file_type: str, component_id: str) -> str:
+    """Save an image or vector file to the server file system"""
     # Validate file type
     if file_type not in ['image', 'vector']:
         raise ValueError("Invalid file type. Must be 'image' or 'vector'.")
@@ -12,31 +13,34 @@ def save_image_file(sortiment: str, file_object, file_type: str, component_id: s
         raise ValueError("Sortiment does not exist in the database.")
 
     # Validate file file extension
-    ext = os.path.splitext(file_object.name)[1].lower()
+    file_ext = os.path.splitext(file_object.name)[1].lower()
     if file_type == 'image':
-        if ext not in ['.jpg', '.jpeg', '.png']:
+        if file_ext not in ['.jpg', '.jpeg', '.png']:
             raise ValueError("Unsupported file extension for image")
     elif file_type == 'vector':
-        if ext not in ['.svg', '.jpg', '.jpeg', '.png']:
+        if file_ext not in ['.svg', '.jpg', '.jpeg', '.png']:
             raise ValueError("Unsupported file extension for vector")
     else:
         raise ValueError("Unsupported file type")
 
     # Use component_id as filename
-    filename = f"{component_id}{ext}"
+    filename = f"{component_id}{file_ext}"
     
     dir_path = os.path.join(settings.MEDIA_ROOT, sortiment, file_type)
     file_path = os.path.join(dir_path, filename)
 
-    # Create the directory and save the file
+    # Create the directory
     os.makedirs(dir_path, exist_ok=True)
     
     # Remove existing file with different extension if it exists 
     for existing_ext in ['.jpg', '.jpeg', '.png', '.svg']:
-        if existing_ext != ext:
+        if existing_ext != file_ext:
             existing_file = os.path.join(dir_path, f"{component_id}{existing_ext}")
             if os.path.exists(existing_file):
                 os.remove(existing_file)
+    
+    # Optimize the file
+    file_object = optimize_image(file_object, file_ext)
     
     # Save as new file
     with open(file_path, 'wb+') as file:
@@ -45,3 +49,21 @@ def save_image_file(sortiment: str, file_object, file_type: str, component_id: s
 
     # Return the URL of the saved file
     return f"{settings.MEDIA_URL}{sortiment}/{file_type}/{filename}"
+
+def optimize_image(file_object, file_ext: str):
+    """Optimize file for optimal file size"""
+    file_ext = file_ext.removeprefix('.')
+    
+    # Check which file extension we are working with and optimize file
+    if file_ext == 'png':
+        image = Image.open(file_object)
+        image.save(image, format='png', optimize=True)
+        return open(image, 'rb')
+    
+    elif file_ext in ['jpg', 'jpeg']:
+        image = Image.open(file_object)
+        image.save(image, format='jpeg', optimize=True)
+        return open(image, 'rb')
+    
+    elif file_ext == 'svg':
+        return file_object
