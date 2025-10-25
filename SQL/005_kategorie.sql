b7f07dd71c8b86c7543ebf1e872484a56cf3d06d
Jakub Štefček <jakub.stefcek@hotmail.cz>
2025-10-25 23:07:57 +0200

    Refactor Nginx configuration: separate HTTP/2 directive and comment out SSL stapling settings

--- SQL/005_kategorie.sql
+++ SQL/005_kategorie.sql
@@ -4,4 +4,4 @@ INSERT INTO goldfren_data.c_kategorie (kod,ikona,nazev,nazev_eng) VALUES
 	 (3,'bicycle','Kolo','Bicycle'),
 	 (4,'plane','Letadlo','Aeroplane'),
 	 (5,'cogs','Průmysl','Industrial'),
-	 (6,'flag-checkered','Motokára','Kart');
+	 (6,'flag-checkered','Motokáry','Kart');
